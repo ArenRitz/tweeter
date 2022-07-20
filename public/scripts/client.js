f5f4c22f4ebf90268795d3c84b43bc402063bb6a
ArenRitz <aren.dolbakian@gmail.com>
2022-07-20 15:09:40 -0400

    Tweets show time since it was posted and prevent html from running in tweet content

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,80 +4,112 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
 $(document).ready(function () {
+  //
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-  
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
-  
-  
-  
-  
-  
   const createTweetElement = function (newTweet) {
-    let content = newTweet.content.text;
+    let content = escape(newTweet.content.text);
     let username = newTweet.user.name;
     let handle = newTweet.user.handle;
-    let utc = newTweet.created_at;
-    let date = new Date(0)
-    date.setUTCSeconds(utc);
-    const tweet = `
-  <article class="tweet-base">
-  <header>
-    <div class="user">
-      <p> <i class="fa-brands fa-bilibili"></i> &nbsp;${username} </p>
-   </div>
-    <h4>${handle}</h4>
-  </header>
-  <div class="tweet-content">
-    <p >${content}</p>
-  </div>  
-  <footer>
-    <span>${date}</span>
-    <div class='tweet-reactions'>
-      <i class="fas fa-flag" id="icon-flag"></i>
-      <i class="fas fa-retweet" id="icon-retweet"></i>
-      <i class="fas fa-heart" id="icon-heart"></i>
-    </div>
-  </footer>
-</article>`;
+    let date = timeago.format(newTweet.created_at);
+    const tweet = $(`
+                  <article class="tweet-base">
+                    <header>
+                      <div class="user">
+                        <p> <i class="fa-brands fa-bilibili"></i> &nbsp;${username} </p>
+                      </div>
+                      <h4>${handle}</h4>
+                    </header>
+                    <div class="tweet-content">
+                      <p >${content}</p>
+                    </div>  
+                    <footer>
+                      <span>${date}</span>
+                      <div class='tweet-reactions'>
+                        <i class="fas fa-flag" id="icon-flag"></i>
+                        <i class="fas fa-retweet" id="icon-retweet"></i>
+                        <i class="fas fa-heart" id="icon-heart"></i>
+                      </div>
+                    </footer>
+                  </article>`);
 
     return tweet;
 
   }
 
-const renderTweets = function (db) { 
-  for (let tweet of db) {
-    const $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
+  const renderTweets = function (db) {
+    $('#tweets-container').empty();
+
+    for (let tweet of db) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
   }
-}
+
+  $(".new-tweet-form").submit(function (event) {
+    event.preventDefault();
+    let userInput = event.target[0].value;
+
+    //test input
+    // console.log(userInput)
+    // console.log('-----')
+    // console.log($(this).serialize())
+    // console.log('-------')
+    // console.log($(this))
+    
+    //form validation
+    if (!userInput) {
+      alert("Please enter a tweet");
+      return;
+    }
+
+    if (userInput.length > 140) {
+      alert("Tweet is too long");
+      return;
+    }
 
 
-renderTweets(data);
+
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: $(this).serialize()
+    }).then(function () {
+      //reset form
+      event.target[0].value = "";
+      $('.counter').text(140);
+      //re-load tweets
+      loadTweets()
+    })
+  });
+
+
+
+
+
+
+
+  const loadTweets = function () {
+
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+    })
+      .then(function (tweet) {
+        renderTweets(tweet);
+      });
+
+  }
+  loadTweets();
+
 
 
 
